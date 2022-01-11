01d4fdcd06feee53bef3732c196a16c1ed1617b2
max-ima <96241406+max-ima@users.noreply.github.com>
2022-01-11 11:23:34 +0100

    sl_SI
    Added slovenian translation

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,59 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Add Index - a Piwigo Plugin                                           |
+// | Copyright (C) 2019-2011 Piwigo team                                   |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Configuration'] = 'Konfiguracija';
+$lang['Photo from Email'] = 'Slike iz e-pošte';
+
+//config
+$lang['Add new mailbox'] = 'Dodajanje novega poštnega predala';
+$lang['Path'] = 'Pot';
+$lang['show examples'] = 'pokaži primere';
+$lang['use it'] = 'uporabi';
+$lang['Login'] = 'Prijava';
+$lang['Password'] = 'Geslo';
+$lang['Where?'] = 'Kje?';
+$lang['Moderation'] = 'Moderiranje';
+$lang['path'] = 'pot';
+$lang['login'] = 'prijava';
+$lang['password'] = 'geslo';
+$lang['album'] = 'album';
+$lang['moderated'] = 'moderirano';
+$lang['an administrator must approve each photo submitted'] = 'skrbnik mora odobriti vsako poslano sliko';
+$lang['Cancel'] = 'Prekliči';
+$lang['Test mailbox'] = 'Preizkusi poštni predal';
+$lang['Mailboxes'] = 'Poštni predali';
+$lang['Edit'] = 'Uredi';
+$lang['Delete'] = 'Izbriši';
+$lang['Notification'] = 'Obveščanje';
+$lang['Notify groups on new photos'] = 'Obvesti skupine o novih slikah';
+$lang['Type in a search term'] = 'Vnesite iskalni izraz';
+$lang['There is no group in this gallery.'] = 'V tej galeriji ni skupine.';
+$lang['Group management'] = 'Upravljanje skupine';
+$lang['Save Settings'] = 'Shrani nastavitve';
+
+//pendings
+$lang['Pending Photos'] = 'Slike na čakanju';
+$lang['Photo from Email'] = 'Slike iz e-pošte';
+$lang['Validate'] = 'Potrdi';
+$lang['Reject'] = 'Zavrni';
+$lang['Zoom'] = 'Povečava';
+$lang['Name'] = 'Ime';
+$lang['Created on'] = 'Ustvarjeno na';
+?>
