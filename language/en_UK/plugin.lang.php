ca9392eae350c5e6a489bcab073d2de2ba2588b0
max-ima <96241406+max-ima@users.noreply.github.com>
2022-01-11 11:22:29 +0100

    English
    In english translation

---
+++ language/en_UK/plugin.lang.php
@@ -0,0 +1,59 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Add Index - a Piwigo Plugin                                           |
+// | Copyright (C) 2019-2011 Piwigo team                                   |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Configuration'] = 'Configuration';
+$lang['Photo from Email'] = 'Photo from Email';
+
+//config
+$lang['Add new mailbox'] = 'Add new mailbox';
+$lang['Path'] = 'Path';
+$lang['show examples'] = 'show examples';
+$lang['use it'] = 'use it';
+$lang['Login'] = 'Login';
+$lang['Password'] = 'Password';
+$lang['Where?'] = 'Where?';
+$lang['Moderation'] = 'Moderation';
+$lang['an administrator must approve each photo submitted'] = 'an administrator must approve each photo submitted';
+$lang['Cancel'] = 'Cancel';
+$lang['Test mailbox'] = 'Test mailbox';
+$lang['Mailboxes'] = 'Mailboxes';
+$lang['path'] = 'path';
+$lang['login'] = 'login';
+$lang['password'] = 'password';
+$lang['album'] = 'album';
+$lang['moderated'] = 'moderated';
+$lang['Edit'] = 'Edit';
+$lang['Delete'] = 'Delete';
+$lang['Notification'] = 'Notification';
+$lang['Notify groups on new photos'] = 'Notify groups on new photos';
+$lang['Type in a search term'] = 'Type in a search term';
+$lang['There is no group in this gallery.'] = 'There is no group in this gallery.';
+$lang['Group management'] = 'Group management';
+$lang['Save Settings'] = 'Save Settings';
+
+//pendings
+$lang['Pending Photos'] = 'Pending Photos';
+$lang['Photo from Email'] = 'Photo from Email';
+$lang['Validate'] = 'Validate';
+$lang['Reject'] = 'Reject';
+$lang['Zoom'] = 'Zoom';
+$lang['Name'] = 'Name';
+$lang['Created on'] = 'Created on';
+?>
